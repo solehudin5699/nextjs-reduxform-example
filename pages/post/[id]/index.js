4ad72cd74a9760dbfe86bc2d00de34d3de62a825
solehudin5699 <solehudinamqi@yahoo.co.id>
2021-03-01 14:39:33 +0700

    add api

--- pages/post/[id]/index.js
+++ pages/post/[id]/index.js
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
+import useSWR from "swr";
+
+const fetcher = async (url) => {
+  const res = await fetch(url);
+  const data = await res.json();
+
+  if (res.status !== 200) {
+    throw new Error(data.message);
+  }
+  return data;
+};
 
 export default function Home() {
   const router = useRouter();
   const { id } = router.query;
+  const { data, error } = useSWR("/api/hello", fetcher);
+
+  console.log(data);
   return (
     <div>
-      <h1>Heloo World {id} </h1>
+      <h1>Data {id} </h1>
+      <div>
+        <ul>
+          {data && data.map((person) => <li key={person.id}>{person.name}</li>)}
+        </ul>
+      </div>
     </div>
   );
 }
